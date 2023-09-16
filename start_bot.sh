d6f9febe493396d898905c329ccb4bd438aa0dcf
StanislavOdnorog <isodnorog@gmail.com>
2023-09-16 21:44:12 +0000

    stylefix: minor wait fix in script

--- start_bot.sh
+++ start_bot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-nohup python src/bot.py >> .bot_log
-nohup python src/bot.py -s >> .support_log
-nohup python src/bot.py -u >> .update_log
+nohup python src/bot.py >> .bot_log &
+nohup python src/bot.py -s >> .support_log &
+nohup python src/bot.py -u >> .update_log &
 
+sleep 1
 ps -fe | grep bot.py
