f32fbb898f7dae77ea8ac71e2e58ea2ba02b412e
StanislavOdnorog <isodnorog@gmail.com>
2023-09-16 21:29:27 +0000

    fix: fix namings in starting script

--- start_bot.sh
+++ start_bot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-source venv/bin/activate
-
 nohup python src/bot.py >> .bot_log
-nohup python src/bot.py -s >> .suport_log
+nohup python src/bot.py -s >> .support_log
 nohup python src/bot.py -u >> .update_log
 
 ps -fe | grep bot.py
